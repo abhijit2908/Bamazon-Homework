0fee11aa5068c3df3daf3672d6934943c37dd2e4
Abhijit <abhijitkghosh29@gmail.com>
2017-11-13 23:44:29 -0500

    updating sql

--- customerview.sql
+++ customerview.sql
@@ -17,12 +17,12 @@ VALUES
    ("L Section Sofa", "Furniture", 1999.99, 100),
     ("Papasan Chair", "Furniture", 69.99, 1000),
     ("Lounge Chair", "Furniture", 799.99, 2000),
-    ("Dinner Set", "Cookware", 579.99, 200),
-    ("Glassware", "Dining", 34.99, 500),
-    ("Comforter", "Bedding", 57.99, 350),
-    ("Plush Blanket", "Bedding", 19.99, 400),
-    ("Chocolates", "Desserts", 19.99, 400),
-     ("Pudding", "Desserts", 29.99, 600),
-     ("Muffins", "Desserts", 6.99, 1400);
+    ("Dinner Set", "Cookware", 579.99, 2000),
+    ("Glassware", "Dining", 34.99, 5000),
+    ("Comforter", "Bedding", 57.99, 3500),
+    ("Plush Blanket", "Bedding", 19.99, 4000),
+    ("Chocolates", "Desserts", 19.99, 4000),
+     ("Pudding", "Desserts", 29.99, 6000),
+     ("Muffins", "Desserts", 6.99, 14000);
      
  select * from Products;    
